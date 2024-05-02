0ae6820097161ade78f70e15b5eb1e37a9f89709
shindonghyuk <tlsehdgur42@naver.com>
2024-05-02 18:19:12 +0900

    리액트 달력 만들기 2024_05_02

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Calendar from './pages/plan/Calendar';
+
+const App = () => {
+  return (
+    <Calendar />
+  );
+}
+
+export default App;
---
+++ src/pages/plan/PopupEvent.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+
+const PopupEvent = ({ clickedDate, onClose }) => {
+  const [title, setTitle] = useState('');
+  const [color, setColor] = useState('#ffffff');
+  const [summary, setSummary] = useState('');
+  const [startingHour, setStartingHour] = useState('');
+  const [endingHour, setEndingHour] = useState('');
+
+  const handleSave = () => {
+    const eventData = {
+      title: title,
+      color: color,
+      date: {...clickedDate},
+      startingHour: startingHour,
+      endingHour: endingHour,
+      summary: summary,
+    };
+
+    axios.post('http://localhost:8080/api/event', eventData)
+    .then(response => {
+      console.log('캘린더 일정이 성공적으로 저장되었습니다.');
+      onClose(); // 저장 후 팝업 닫기
+    })
+    .catch(error => {
+      console.error('캘린더 일정 저장에 실패했습니다.', error);
+    });
+
+  };
+
+  return (
+    <div className="popup">
+      <div className="popup-content">
+        <span className="close" onClick={onClose}>&times;</span>
+        <h2>일정 넣기</h2>
+        <label htmlFor="title">제목:</label>
+        <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <p>클릭한 날짜: {clickedDate.year}. {clickedDate.month}. {clickedDate.date}</p> {/* 클릭한 날짜 출력 */}
+        <label htmlFor='startingHour'>시작 시간:</label>
+        <input type='time' id='startingHour' value={startingHour} onChange={(e) => setStartingHour(e.target.value)}/>
+        <label htmlFor='endingHour'>끝 시간:</label>
+        <input type='time' id='endingHour' value={endingHour} onChange={(e) => setEndingHour(e.target.value)}/>
+        <br/>
+        <label htmlFor="color">색상:</label>
+        <input type="color" id="color" value={color} onChange={(e) => setColor(e.target.value)} />
+        <br/>
+        <label htmlFor="summary">메모:</label>
+        <input type="text" id="summary" value={summary} onChange={(e) => setSummary(e.target.value)} />
+        <button onClick={handleSave}>저장</button>
+      </div>
+    </div>
+  );
+};
+
+export default PopupEvent;
+
